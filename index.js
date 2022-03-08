062e1b5f0b8883d2275038836813b37b1ebc4383
JShinault0620 <jshincool@gmail.com>
2022-03-08 16:10:00 -0500

    Added images and character movement

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+const moveElement = (string, distance, negative = false) => {
+    let number = Number(string.slice(0, -2))
+    if (negative) {
+        number -= distance
+    } else {
+        number += distance
+    }
+    return String(number + 'px')
+}
+
+const createImage = (name, left, bottom, src, character = false) => {
+    let newImage = document.createElement('img')
+    newImage.src = src
+    newImage.style.position = 'fixed'
+    newImage.style.left = left
+    newImage.style.bottom = bottom
+    newImage.id = name
+    if (character) {
+        console.log(newImage.style.bottom)
+        document.addEventListener('keypress', (event) => {
+            switch (event.key) {
+                case 'w':
+                    newImage.style.bottom = moveElement(newImage.style.bottom, 25)
+                    break;
+                case 'a':
+                    newImage.style.left = moveElement(newImage.style.left, 25, true)
+                    break;
+                case 's':
+                    newImage.style.bottom = moveElement(newImage.style.bottom, 25, true)
+                    break;
+                case 'd':
+                    newImage.style.left = moveElement(newImage.style.left, 25)
+                    break;
+            }
+        })
+    } else {
+        newImage.addEventListener('click', () => {
+            newImage.remove()
+        })
+    }
+    document.body.append(newImage)
+    console.log('Created ' + name)
+}
+
+let imageArray = [
+    {name: 'greenCharacter', left: '100px', bottom: '100px', src: 'assets/green-character.gif', character: true},
+    {name: 'pineTree', left: '450px', bottom: '200px', src: 'assets/pine-tree.png'},
+    {name: 'tree', left: '200px', bottom: '300px', src: 'assets/tree.png'},
+    {name: 'pillar', left: '350px', bottom: '100px', src: 'assets/pillar.png'},
+    {name: 'crate', left: '150px', bottom: '200px', src: 'assets/crate.png'},
+    {name: 'well', left: '500px', bottom: '425px', src: 'assets/well.png'},
+    {name: 'sword', left: '500px', bottom: '405px', src: 'assets/sword.png'},
+    {name: 'shield', left: '165px', bottom: '185px', src: 'assets/shield.png'},
+    {name: 'sword', left: '600px', bottom: '100px', src: 'assets/staff.png'}
+]
+
+let body = document.querySelector('body')
+body.style.backgroundColor = 'green'
+
+for (let i = 0; i < imageArray.length; i ++) {
+    createImage(imageArray[i].name, imageArray[i].left, imageArray[i].bottom, imageArray[i].src, imageArray[i].character)
+}
